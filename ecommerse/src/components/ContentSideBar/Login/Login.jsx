0ef148f7f2221568839df95fbabdf3de26f01741
iljimae0410 <iljimae0410@gmail.com>
2025-01-06 18:24:33 +0700

    update formik validate form input

--- ecommerse/src/components/ContentSideBar/Login/Login.jsx
+++ ecommerse/src/components/ContentSideBar/Login/Login.jsx
@@ -1,20 +1,54 @@
 import InputCommon from '@components/InputCommon/InputCommon';
 import styles from './styles.module.scss';
 import Button from '@components/Button/Button';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 function Login() {
     const { container, title, boxRememberMe, lostPw } = styles;
 
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: ''
+        },
+        validationSchema: Yup.object({
+            email: Yup.string()
+                .email('Invalid email')
+                .required('Email is required'),
+            password: Yup.string()
+                .min(6, 'Password must be at least 6 characters')
+                .required('Password is required')
+        }),
+        onSubmit: values => {
+            console.log(values);
+        }
+    });
+
     return (
         <div className={container}>
             <div className={title}>SIGN IN</div>
-            <InputCommon lable='Email' type='text' isRequired />
-            <InputCommon lable='Password' type='password' isRequired />
-            <div className={boxRememberMe}>
-                <input type='checkbox' />
-                <span>Remember me</span>
-            </div>
-            <Button content={'LOGIN'} />
+            <form onSubmit={formik.handleSubmit}>
+                <InputCommon
+                    id='email'
+                    lable='Email'
+                    type='text'
+                    isRequired
+                    formik={formik}
+                />
+                <InputCommon
+                    id='password'
+                    lable='Password'
+                    type='password'
+                    isRequired
+                    formik={formik}
+                />
+                <div className={boxRememberMe}>
+                    <input type='checkbox' />
+                    <span>Remember me</span>
+                </div>
+                <Button content={'LOGIN'} type='submit' />
+            </form>
             <div className={lostPw}>Lost your password</div>
         </div>
     );
--- ecommerse/src/components/InputCommon/InputCommon.jsx
+++ ecommerse/src/components/InputCommon/InputCommon.jsx
@@ -2,8 +2,10 @@ import { useState } from 'react';
 import styles from './styles.module.scss';
 import { FiEye, FiEyeOff } from 'react-icons/fi';
 
-function InputCommon({ lable, type, isRequired = false }) {
-    const { labelInput, boxInput, container, boxIcon } = styles;
+function InputCommon({ lable, type, isRequired = false, ...props }) {
+    const { labelInput, boxInput, container, boxIcon, errMsg } = styles;
+
+    const { formik, id } = props;
 
     const [showPassword, setShowPassword] = useState(false);
 
@@ -16,18 +18,28 @@ function InputCommon({ lable, type, isRequired = false }) {
         setShowPassword(!showPassword);
     };
 
+    const isErr = formik.touched[id] && formik.errors[id];
+    const messageErr = formik.errors[id];
+
     return (
         <div className={container}>
             <div className={labelInput}>
                 {lable} {isRequired && <span>*</span>}
             </div>
             <div className={boxInput}>
-                <input type={isShowTextPassword} />
+                <input
+                    type={isShowTextPassword}
+                    {...props}
+                    onBlur={formik.handleBlur}
+                    onChange={formik.handleChange}
+                    value={formik.values[id]}
+                />
                 {isPassword && (
                     <div className={boxIcon} onClick={handleShowPassword}>
                         {showPassword ? <FiEye /> : <FiEyeOff />}
                     </div>
                 )}
+                {isErr && <div className={errMsg}>{messageErr}</div>}
             </div>
         </div>
     );
